776cd0e5ef2e96d103b555dbde4f65baa3462155
Sofia Varich <kirisona@gmail.com>
2019-07-14 21:35:47 +0300

    slick-slider

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.ba-slider').slick({
+      infinite: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      autoPlay: true,
+      appendDots: '.slider-nav'
+    });
+  });
+
